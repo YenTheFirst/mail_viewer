b4a533d92012a28dad2a61478d10f1f7a8f1568f
T S <YenTheFirst@gmail.com>
2011-06-24 12:33:26 -0700

    add more info to logged emails

--- lib/mail_viewer/action_mailer.rb
+++ lib/mail_viewer/action_mailer.rb
@@ -6,8 +6,9 @@ class ActionMailer::Base < AbstractController::Base
 	@all_calls=[]
 
 	def mail_with_logging(headers={},&block)
-		ActionMailer::Base.all_calls << lambda {mail_without_logging(headers,&block)}
-		mail_without_logging(headers,&block)
+		result = mail_without_logging(headers,&block)
+		ActionMailer::Base.all_calls << {:call => lambda {mail_without_logging(headers,&block)}, :date => DateTime.now, :orig => result}
+		result
 	end
 	alias_method_chain :mail, :logging
 
