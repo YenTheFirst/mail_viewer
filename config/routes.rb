ec3a7a35a5a62abe7169eea392066b5c353f4018
T S <YenTheFirst@gmail.com>
2011-06-24 12:51:09 -0700

    fix routes for show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do |map|
 	get "/mail_view", :to => "mail_view#index"
-	get "/mail_view/:index", :to => "mail_view#show"
+	get "/mail_view/:index", :to => "mail_view#show", :as=>"show_mail"
 end
